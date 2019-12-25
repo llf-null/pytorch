9270465a846112f57eae6402f2bf34fd1bf86e2b
linlifan <1678118310@qq.com>
2019-12-25 20:20:23 +0800

    删掉了注释20191225

--- pytorch/lanenet_test.py
+++ pytorch/lanenet_test.py
@@ -28,65 +28,15 @@ def road_lines_test(image):
     transform=transforms.Compose([transforms.ToTensor()])
     images_tensor = transform(small_img) #转为Tensor
     images_tensor=images_tensor.resize(1,3,80,160)
-    #print(images_tensor)
-#     print(images_tensor.shape)
-    #images_tensor=images_tensor.to(device)
     prediction=model(images_tensor)
     
     prediction=prediction.detach().numpy()
     p = np.squeeze(prediction)
     p*=255
-    
-#     im4=Image.fromarray(p)
-#     im4.show()
-    #因为忘了乘255，全是黑的
-#     prediction*=255
-#     p = np.squeeze(prediction)
-#     pdb.set_trace()
-#     im3=Image.fromarray(p)
-#     im3.show()
     p=np.expand_dims(p,axis=2)
 #     print(p.shape)
     p_RGB = cv2.cvtColor(p, cv2.COLOR_GRAY2RGB)
-#     print(p_RGB.shape)
-#     print(type(p_RGB))
-#     cv2.imshow("output",p_RGB)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#     im3=Image.fromarray(p_RGB)
-#     im3.show()
-    #print(type(prediction))
-    
-    
-#     lanes.recent_fit.append(p_RGB)
-#     if len(lanes.recent_fit)>5:
-#         lanes.recent_fit=lanes.recent_fit[1:]
-#     lanes.avg_fit=np.mean(np.array([i for i in lanes.recent_fit]),axis=0)
-#     blanks=np.zeros_like(lanes.recent_fit).astype(np.uint8)
-#     pdb.set_trace()
-#     lanes_drawn=np.dstack((blanks,lanes.recent_fit,blanks)) 
-
-#     lanes_drawn=np.array(lanes_drawn)
-#     cv2.imshow("output",lanes_drawn)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#     lanes_drawn=Image.fromarray(lanes_drawn,mode='RGB')
-#     lanes_drawn.show()
-#     lane_image=lanes_drawn.resize((852,480),Image.ANTIALIAS)
-#     lanes_drawn=np.squeeze(lanes_drawn) #shape(80,480,3)
-#     lanes_drawn=Image.fromarray(lanes_drawn,mode='RGB')
-#     lanes_drawn.show()
-#     lane_image=lanes_drawn.resize((852,480),Image.ANTIALIAS)
     lane_image=cv2.resize(p_RGB,(852,480),interpolation=cv2.INTER_CUBIC)
-    
-#     pdb.set_trace()
-#     result=cv2.addWeighted(image,160,lane_image,1,0.)
-
-#     cv2.imshow("output",lane_image)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()  
-#     im = Image.fromarray(result)
-#     im.show()
     return lane_image
 lanes=Lanes()
 vid_output='project_video_hy.mp4'
